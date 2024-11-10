985ac1a0634bdb55a41ee5f8c7694acd123b46c9
pallabez <pallabsonowal999@gmail.com>
2024-11-10 12:03:56 +0530

    add e2e tests

--- tests/e2e.test.ts
+++ tests/e2e.test.ts
@@ -1,23 +1,47 @@
 import { readFileSync } from 'fs';
 import { execSync } from "child_process"
+import { glob } from 'glob';
+import { REPLACE_MAP } from '@/constants';
 
-const PLAYGROUND_PATH = "/tests/playground"
-const FIXTURES_PATH = "/tests/fixtures"
+const PLAYGROUND_PATH = 'tests/playground';
 
 describe("attack on barrel", () => {
+  beforeAll(() => {
+    // Assemble
+    execSync('npm run set', { stdio: 'inherit' })
+
+    // TODO: Mock not working
+    jest.spyOn(console, 'log').mockImplementation(() => {});
+
+    // Act
+    execSync('npm run attack', { stdio: 'inherit' })
+  })
+
   describe("non-replaceable", () => {
-    it ("should not replace the barrel import if it is not replaceable", () => {
-      // Assemble
-      execSync('npm run set', { stdio: 'inherit' })
+    it ("should not replace the barrel import if it is not replaceable", async () => {
+      const filePaths = await glob(`${PLAYGROUND_PATH}/replaceability-non-replaceable/**/*.ts`);
 
-      // Act
-      execSync('npm run attack', { stdio: 'inherit' })
+      expect(filePaths.length).not.toBe(0)
+      for (const filePath of filePaths) {
+        const playgroundContent = readFileSync(filePath, 'utf-8');
+        const fixtureContent = readFileSync(filePath.replace("/playground", "/fixtures") , 'utf-8');
 
-      // Assert
-      const playgroundContent = readFileSync(`${PLAYGROUND_PATH}/replaceability-replaceable`, "utf-8");
-      const fixtureContent = readFileSync(`${FIXTURES_PATH}/replaceability-replaceable`, "utf-8");
+        expect(playgroundContent).toBe(fixtureContent)
+      }
+    })
+  })
 
-      expect(playgroundContent).not.toBe(fixtureContent)
+  describe("replaceable", () => {
+    it ("should replace the barrel import if it is replaceable", async () => {
+      const filePaths = await glob(`${PLAYGROUND_PATH}/replaceability-replaceable/**/*.ts`);
+
+      expect(filePaths.length).not.toBe(0)
+      for (const filePath of filePaths) {
+        const playgroundContent = readFileSync(filePath, 'utf-8');
+
+        expect(playgroundContent).not.toContain(`${REPLACE_MAP[0].barrelPath};`)
+        expect(playgroundContent).toContain(`${REPLACE_MAP[0].barrelPath}/`)
+      }
     })
   })
 })
